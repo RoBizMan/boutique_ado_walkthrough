3a6aff9c2e229eacebdc657e0f7d0d52a690c663
Robert Tiffert <robert@tiffert.eu>
2024-09-29 18:02:20 +0000

    Add Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Q4QbDBopDiZJQSS5ZiWD72Tmt2ZOjDC4CucdF87A8vmt6JXX2c92FhuFzKPgs051h4sQDp3yDVmwSyNmti3nY6F00WFVx1WaZ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
